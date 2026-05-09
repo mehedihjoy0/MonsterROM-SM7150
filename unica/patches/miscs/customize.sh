1cd8df350facbe6b98e6e9508701af38e74650b2
ditternation <ditternation@outlook.com>
2026-05-09 13:26:52 +0200

    unica: patches: miscs: route oneui 8.5 handling

--- unica/patches/miscs/customize.sh
+++ unica/patches/miscs/customize.sh
@@ -1,4 +1,17 @@
 SET_PROP_IF_DIFF "vendor" "ro.oem_unlock_supported" "0"
 
-SET_PROP "system" "ro.artisanrom.version" "$UPDATER_VERSION"
-SET_PROP "system" "ro.artisanrom.target" "$TARGET_CODENAME"
+SET_PROP "system" "ro.monsterrom.version" "$ROM_VERSION"
+SET_PROP "system" "ro.monsterrom.updater_version" "$UPDATER_VERSION"
+SET_PROP "system" "ro.monsterrom.timestamp" "$ROM_BUILD_TIMESTAMP"
+SET_PROP "system" "ro.monsterrom.device" "$TARGET_CODENAME"
+SET_PROP "system" "ro.monsterrom.target" "$TARGET_CODENAME"
+
+if [ ! "$(GET_PROP "system" "ro.unica.version")" ]; then
+    SET_PROP "system" "ro.unica.version" "$(GET_PROP "system" "ro.monsterrom.version")"
+fi
+if [ ! "$(GET_PROP "system" "ro.unica.timestamp")" ]; then
+    SET_PROP "system" "ro.unica.timestamp" "$(GET_PROP "system" "ro.monsterrom.timestamp")"
+fi
+if [ ! "$(GET_PROP "system" "ro.unica.device")" ]; then
+    SET_PROP "system" "ro.unica.device" "$(GET_PROP "system" "ro.monsterrom.device")"
+fi
