f590d553dfbcbe0d689bc51744d601de15e780af
kakaoacademy3-code <kakaoacademy.3@gmail.com>
2026-05-10 19:58:34 +0900

    KR

--- target/p3s/installer/customize.sh
+++ target/p3s/installer/customize.sh
@@ -1,11 +1,11 @@
-LOG "- Downloading BL_G998BXXSJHZC2_G998BXXSJHZC2_MQB107295400_REV01_user_low_ship_MULTI_CERT.tar.md5"
+LOG "- Downloading BL_G998NKSSCHZA9_G998NKSSCHZA9_MQB107175299_REV01_user_low_ship_MULTI_CERT.tar.md5"
 DOWNLOAD_FILE \
-    "https://github.com/UN1CA/proprietary_vendor_samsung_exynos2100/releases/download/G998BXXSJHZC2_XEO_OXM/BL_G998BXXSJHZC2_G998BXXSJHZC2_MQB107295400_REV01_user_low_ship_MULTI_CERT.tar.md5" \
-    "$TMP_DIR/BL_G998BXXSJHZC2_G998BXXSJHZC2_MQB107295400_REV01_user_low_ship_MULTI_CERT.tar.md5" || return 1
-LOG "- Downloading CP_G998BXXSJHZA6_CP32677266_MQB105755821_REV01_user_low_ship_MULTI_CERT.tar.md5"
+    "https://github.com/UN1CA/proprietary_vendor_samsung_exynos2100/releases/download/G998NKSSCHZA9_KOO_OKR/BL_G998NKSSCHZA9_G998NKSSCHZA9_MQB107175299_REV01_user_low_ship_MULTI_CERT.tar" \
+    "$TMP_DIR/BL_G998NKSSCHZA9_G998NKSSCHZA9_MQB107175299_REV01_user_low_ship_MULTI_CERT.tar.md5" || return 1
+LOG "- Downloading CP_G998NKOSCHZA5_CP32602500_MQB105441885_REV01_user_low_ship_MULTI_CERT.tar.md5"
 DOWNLOAD_FILE \
-    "https://github.com/UN1CA/proprietary_vendor_samsung_exynos2100/releases/download/G998BXXSJHZC2_XEO_OXM/CP_G998BXXSJHZA6_CP32677266_MQB105755821_REV01_user_low_ship_MULTI_CERT.tar.md5" \
-    "$TMP_DIR/CP_G998BXXSJHZA6_CP32677266_MQB105755821_REV01_user_low_ship_MULTI_CERT.tar.md5" || return 1
+    "https://github.com/UN1CA/proprietary_vendor_samsung_exynos2100/releases/download/G998NKSSCHZA9_KOO_OKR/CP_G998NKOSCHZA5_CP32602500_MQB105441885_REV01_user_low_ship_MULTI_CERT.tar.md5MQB107175299_REV01_user_low_ship_MULTI_CERT.tar.md5" \
+    "$TMP_DIR/CP_G998NKOSCHZA5_CP32602500_MQB105441885_REV01_user_low_ship_MULTI_CERT.tar.md5" || return 1
 
 while IFS= read -r f; do
     FILE_NAME="$(basename "$f")"
