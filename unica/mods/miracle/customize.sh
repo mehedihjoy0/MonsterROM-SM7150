9fa9435c13b362aa39ef27e106011088a762c5ff
ditternation <ditternation@outlook.com>
2026-06-06 17:18:02 +0200

    adjustments

--- unica/mods/miracle/customize.sh
+++ unica/mods/miracle/customize.sh
@@ -11,11 +11,6 @@ SET_FLOATING_FEATURE_CONFIG "SEC_FLOATING_FEATURE_COMMON_CONFIG_AI_VERSION" "202
 
 if ! $TARGET_AUDIO_SUPPORT_ACH_RINGTONE; then
     LOG_STEP_IN "- Adding non-ACH audio fallback"
-    DELETE_FROM_WORK_DIR "system" "system/media/audio/notifications"
-    DELETE_FROM_WORK_DIR "system" "system/media/audio/ringtones"
-    ADD_TO_WORK_DIR "a56xnaxx" "system" "system/etc/ringtones_count_list.txt" 0 0 644 "u:object_r:system_file:s0"
-    ADD_TO_WORK_DIR "a56xnaxx" "system" "system/media/audio/notifications" 0 0 755 "u:object_r:system_file:s0"
-    ADD_TO_WORK_DIR "a56xnaxx" "system" "system/media/audio/ringtones" 0 0 755 "u:object_r:system_file:s0"
     SET_PROP "vendor" "ro.config.ringtone" "Galaxy_Bells.ogg"
     SET_PROP "vendor" "ro.config.notification_sound" "Brightline.ogg"
     SET_PROP "vendor" "ro.config.alarm_alert" "Morning_Xylophone.ogg"
