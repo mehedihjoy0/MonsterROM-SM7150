eff7031dc9cf57787a0d6b80d50b0e2beff8dd42
Mehedi H Joy <78579303+mehedihjoy0@users.noreply.github.com>
2026-06-07 02:50:47 +0600

    Update customize.sh

--- target/m51/patches/miscs/customize.sh
+++ target/m51/patches/miscs/customize.sh
@@ -2,3 +2,5 @@ LOG_STEP_IN "- Increasing speaker volume values to 90"
 EVAL "sed -i -E '/WSA_RX[01] Digital Volume/s/value=\"[^\"]*\"/value=\"90\"/' \
 \"$WORK_DIR\"/vendor/etc/mixer_paths*.xml"
 LOG_STEP_OUT
+
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system_ext" "etc/selinux/mapping/30.0.cil"
