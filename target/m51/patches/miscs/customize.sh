27c430e7c8925737dafcbe73afbbe8cbfc4e5036
Mehedi H Joy <78579303+mehedihjoy0@users.noreply.github.com>
2026-06-07 09:54:12 +0600

    Update customize.sh

--- target/m51/patches/miscs/customize.sh
+++ target/m51/patches/miscs/customize.sh
@@ -3,4 +3,4 @@ EVAL "sed -i -E '/WSA_RX[01] Digital Volume/s/value=\"[^\"]*\"/value=\"90\"/' \
 \"$WORK_DIR\"/vendor/etc/mixer_paths*.xml"
 LOG_STEP_OUT
 
-ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system_ext" "etc/selinux/mapping/30.0.cil"
+#ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system_ext" "etc/selinux/mapping/30.0.cil"
