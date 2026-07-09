3e7355ecb1b7df6f3952e475dcbde36e7c58d78b
devcore94 <ditternation@outlook.com>
2026-07-09 14:10:18 +0200

    split change 0004/0862: platform/exynos2100/patches/camera/customize.sh
    Signed-off-by: devcore94 <ditternation@outlook.com>

--- platform/exynos2100/patches/camera/customize.sh
+++ platform/exynos2100/patches/camera/customize.sh
@@ -1,3 +1,7 @@
+
+
+# Add ImageTagger lib
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libImageTagger.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
 # Add Polarr libs
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/etc/public.libraries-polarr.txt" 0 0 644 "u:object_r:system_file:s0"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libPolarrSnap.polarr.so" 0 0 644 "u:object_r:system_lib_file:s0"
@@ -9,10 +13,6 @@ ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libdualcam_portraitlig
 
 # Add Snap libs
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/etc/public.libraries-snap.samsung.txt" 0 0 644 "u:object_r:system_file:s0"
-ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libeden_wrapper_system.so" 0 0 644 "u:object_r:system_lib_file:s0"
-DELETE_FROM_WORK_DIR "system" "system/lib64/libenn_wrapper_system.so"
-ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libsecuresnap_aidl.snap.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
-ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libsnap_aidl.snap.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
 
 # Add camera libs
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/etc/public.libraries-arcsoft.txt" 0 0 644 "u:object_r:system_file:s0"
@@ -55,3 +55,58 @@ fi
 
 LOG "- Fixing MIDAS model detection"
 EVAL "sed -i \"s/$TARGET_CODENAME/r0s/g\" \"$WORK_DIR/vendor/etc/midas/midas_config.json\""
+LOG_STEP_IN "- Replacing camera blobs"
+BLOBS_LIST="
+system/lib64/libae_bracket_hdr.arcsoft.so
+system/lib64/libarcsoft_dualcam_portraitlighting.so
+system/lib64/libdualcam_refocus_gallery_48.so
+system/lib64/libdualcam_refocus_gallery_59.so
+system/lib64/libDualCamBokehCapture.camera.samsung.so
+system/lib64/libface_recognition.arcsoft.so
+system/lib64/libgallery_pic_best.arcsoft.so
+system/lib64/libhybrid_high_dynamic_range.arcsoft.so
+"
+for blob in $BLOBS_LIST
+do
+    DELETE_FROM_WORK_DIR "system" "$blob" &
+done
+
+# shellcheck disable=SC2046
+wait $(jobs -p) || exit 1
+
+BLOBS_LIST="
+system/lib64/libDocShadowRemoval.arcsoft.so
+system/lib64/libeden_wrapper_system.so
+system/lib64/libhigh_dynamic_range.arcsoft.so
+system/lib64/libhigh_res.arcsoft.so
+system/lib64/libImageSegmenter_v1.camera.samsung.so
+system/lib64/liblow_light_hdr.arcsoft.so
+system/lib64/libMultiFrameProcessing30.camera.samsung.so
+system/lib64/libMultiFrameProcessing30.snapwrapper.camera.samsung.so
+system/lib64/libMultiFrameProcessing30Tuning.camera.samsung.so
+system/lib64/libObjectDetector_v1.camera.samsung.so
+system/lib64/libPortraitDistortionCorrectionCali.arcsoft.so
+system/lib64/libSceneDetector_v1.camera.samsung.so
+system/lib64/libsecuresnap_aidl.snap.samsung.so
+system/lib64/libsnap_aidl.snap.samsung.so
+system/lib64/libsuperresolution_wrapper_v2.camera.samsung.so
+system/lib64/libsuperresolution.arcsoft.so
+system/lib64/libSwIsp_core.camera.samsung.so
+system/lib64/libSwIsp_wrapper_v1.camera.samsung.so
+system/lib64/libVideoClassifier.camera.samsung.so
+"
+if [[ "$TARGET_CODENAME" == "p3s" ]]; then
+    BLOBS_LIST+="
+    system/lib64/libsuperresolution_raw.arcsoft.so
+    system/lib64/libsuperresolutionraw_wrapper_v2.camera.samsung.so
+    system/lib64/libuwsuperresolution.arcsoft.so
+    system/lib64/libuwsuperresolution_wrapper_v1.camera.samsung.so
+    "
+fi
+for blob in $BLOBS_LIST
+do
+    ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "$blob" 0 0 644 "u:object_r:system_lib_file:s0" &
+done
+wait $(jobs -p) || exit 1
+
+
