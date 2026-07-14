5f3c6f3a4192fb7990f21ff8c4cbca5fe27c3e83
ditternation <ditternation@outlook.com>
2026-07-14 07:15:25 +0200

    split change 0115/0118: target/dm1q/sff.sh
    Signed-off-by: ditternation <ditternation@outlook.com>

---
+++ target/dm1q/sff.sh
@@ -0,0 +1,7 @@
+# Copyright (c) 2026 ditternation
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+# SEC Floating Feature configuration file for Galaxy S23 (dm1q)
+
+# Enable S26 Ultra privacy display framework support
+SEC_FLOATING_FEATURE_LCD_CONFIG_PRIVACY_DISPLAY=1
