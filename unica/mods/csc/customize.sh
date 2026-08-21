e4998989306032708177551eb6b1e2a3d591c29f
Devcore94 <devcore94@users.noreply.github.com>
2026-08-22 01:47:02 +0200

    unica: mods: csc: move QPR2 remote management to smali patch

--- unica/mods/csc/customize.sh
+++
@@ -1,4 +0,0 @@
-# Hide Remote management tile in Settings app
-SMALI_PATCH "system" "system/priv-app/SecSettings/SecSettings.apk" \
-    "smali_classes4/com/samsung/android/settings/homepage/TopLevelRemoteSupportPreferenceController.smali" "return" \
-    'getAvailabilityStatus()I' '3'
